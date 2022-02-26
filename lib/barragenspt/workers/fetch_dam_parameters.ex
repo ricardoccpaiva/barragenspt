9599bbc93f11e73b52e9b3213f30f9f4ea2041f1
Ricardo Paiva <ricardoccpaiva@gmail.com>
2022-02-26 23:32:04 +0000

    Replace worker to store date in PG instead of influx

--- lib/barragenspt/workers/fetch_dam_parameters.ex
+++ lib/barragenspt/workers/fetch_dam_parameters.ex
@@ -2,19 +2,9 @@ defmodule Barragenspt.Workers.FetchDamParameters do
   use Oban.Worker, queue: :dams_info
   import Ecto.Query
   require Logger
+  alias Barragenspt.Hydrometrics.DataPoint
 
   @timeout 25000
-
-  def stuff do
-    from(Barragenspt.Hydrometrics.Dam)
-    |> Barragenspt.Repo.all()
-    |> Enum.each(fn dam ->
-      total_volume = dam.metadata["Albufeira"]["Capacidade total (dam3)"]
-      max_quota = dam.metadata["Albufeira"]["Cota do nível de pleno armazenamento - NPA (m)"]
-      IO.puts(~s(total_volume ----> #{total_volume}))
-      IO.puts(~s(max_quota ----> #{max_quota}))
-    end)
-  end
 
   def spawn_workers do
     # 354895424 - Cota da Albufeira na última hora
@@ -25,27 +15,35 @@ defmodule Barragenspt.Workers.FetchDamParameters do
       {354_895_424, "quota_last_hour"},
       {1_629_599_726, "quota"},
       {1_629_599_798, "volume"},
-      {354_895_398, "volume_last_hour"}
+      {354_895_398, "volume_last_hour"},
+      {304_545_050, "volume_last_day_month"}
     ]
 
     # data_params = [{354_895_398, "volume_last_hour"}]
     # data_params = [{1_629_599_798, "volume"}]
 
+    years = [
+      {1990, 2000},
+      {2001, 2010},
+      {2011, 2022}
+    ]
+
     from(Barragenspt.Hydrometrics.Dam)
     |> Barragenspt.Repo.all()
     |> Enum.map(fn dam ->
       Enum.map(data_params, fn {param_id, param_name} ->
-        Barragenspt.Workers.FetchDamParameters.new(%{
-          "id" => :rand.uniform(999_999_999),
-          "dam_code" => dam.code,
-          "basin_id" => dam.basin_id,
-          "site_id" => dam.site_id,
-          "parameter_id" => param_id,
-          "parameter_name" => param_name,
-          "total_volume" => dam.metadata["Albufeira"]["Capacidade total (dam3)"],
-          "max_quota" =>
-            dam.metadata["Albufeira"]["Cota do nível de pleno armazenamento - NPA (m)"]
-        })
+        Enum.map(years, fn {start_year, end_year} ->
+          Barragenspt.Workers.FetchDamParameters.new(%{
+            "id" => :rand.uniform(999_999_999),
+            "dam_code" => dam.code,
+            "basin_id" => dam.basin_id,
+            "site_id" => dam.site_id,
+            "parameter_id" => param_id,
+            "parameter_name" => param_name,
+            "start_year" => start_year,
+            "end_year" => end_year
+          })
+        end)
       end)
     end)
     |> List.flatten()
@@ -62,8 +60,8 @@ defmodule Barragenspt.Workers.FetchDamParameters do
             "basin_id" => basin_id,
             "parameter_id" => parameter_id,
             "parameter_name" => parameter_name,
-            "total_volume" => total_volume,
-            "max_quota" => max_quota
+            "start_year" => start_year,
+            "end_year" => end_year
           } = _args
       }) do
     :timer.sleep(4000)
@@ -72,7 +70,7 @@ defmodule Barragenspt.Workers.FetchDamParameters do
     File.mkdir("resources/tmp/job_#{job_id}")
 
     site_id
-    |> get_raw_csv(parameter_id)
+    |> get_raw_csv(parameter_id, start_year, end_year)
     |> write_to_file()
     |> File.stream!()
     |> NimbleCSV.RFC4180.parse_stream()
@@ -87,14 +85,12 @@ defmodule Barragenspt.Workers.FetchDamParameters do
           basin_id,
           parameter_name,
           parameter_id,
-          total_volume,
-          max_quota,
           row_item
         )
       end)
+      |> List.flatten()
       |> Enum.reject(fn row -> row == :noop end)
-      |> Enum.join("\n")
-      |> save_to_influx_cloud()
+      |> save_rows()
     end)
     |> Stream.run()
 
@@ -103,11 +99,11 @@ defmodule Barragenspt.Workers.FetchDamParameters do
     :ok
   end
 
-  defp get_raw_csv(site_id, parameter_id) do
+  defp get_raw_csv(site_id, parameter_id, start_year, end_year) do
     base_url = "https://snirh.apambiente.pt/snirh/_dadosbase/site/paraCSV/dados_csv.php"
 
     query_params =
-      "?sites=#{site_id}&pars=#{parameter_id}&tmin=01/08/2010&tmax=31/01/2022&formato=csv"
+      "?sites=#{site_id}&pars=#{parameter_id}&tmin=01/01/#{start_year}&tmax=31/12/#{end_year}&formato=csv"
 
     options = [recv_timeout: @timeout, timeout: @timeout]
     %HTTPoison.Response{body: body} = HTTPoison.get!(base_url <> query_params, [], options)
@@ -137,8 +133,6 @@ defmodule Barragenspt.Workers.FetchDamParameters do
          basin_id,
          parameter_name,
          parameter_id,
-         total_volume,
-         max_quota,
          [
            date,
            value,
@@ -157,69 +151,36 @@ defmodule Barragenspt.Workers.FetchDamParameters do
       "year" => year
     } = Regex.named_captures(regex, date)
 
-    ts =
+    dt =
       "#{year}-#{month}-#{day}T#{hour}:#{minutes}:00Z"
       |> DateTime.from_iso8601()
       |> then(fn {:ok, dt, 0} -> dt end)
-      |> DateTime.to_unix()
 
     sanitized_param_name =
       parameter_name
       |> String.downcase()
       |> String.replace(" ", "_")
 
-    value_pct =
-      case parameter_name do
-        "volume" -> calculate_pct(value, total_volume)
-        "volume_last_hour" -> calculate_pct(value, total_volume)
-        "quota" -> calculate_pct(value, max_quota)
-        "quota_last_hour" -> calculate_pct(value, max_quota)
-      end
+    date = NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
 
-    "#{sanitized_param_name}_pct,dam_code=#{dam_code},basin_id=#{basin_id},site_id=#{site_id} value=#{value_pct} #{ts}\n" <>
-      "#{sanitized_param_name},dam_code=#{dam_code},basin_id=#{basin_id},site_id=#{site_id},parameter_id=#{parameter_id} value=#{value} #{ts}"
-  end
-
-  defp handle_row(_, _, _, _, _, _, _, _), do: :noop
-
-  defp calculate_pct(value, total_value) do
     {float_val, _else} = Float.parse(value)
-    {float_total_val, _else} = Float.parse(total_value)
-    Float.ceil(float_val / float_total_val * 100, 2)
+
+    %{
+      param_name: sanitized_param_name,
+      param_id: to_string(parameter_id),
+      dam_code: dam_code,
+      basin_id: to_string(basin_id),
+      site_id: site_id,
+      value: float_val,
+      inserted_at: date,
+      updated_at: date,
+      colected_at: NaiveDateTime.truncate(dt, :second)
+    }
   end
 
-  defp save_to_influx(payload) do
-    headers = [{"Content-Type", "text/plain; charset=utf-8"}]
+  defp handle_row(_, _, _, _, _, _), do: :noop
 
-    %HTTPoison.Response{status_code: 204} =
-      HTTPoison.post!(
-        "http://pi4:8086/api/v2/write?bucket=barragenspt&precision=s",
-        payload,
-        headers
-      )
-
-    # :timer.sleep(50)
-
-    :ok
-  end
-
-  defp save_to_influx_cloud(payload) do
-    token = System.get_env("INFLUX_CLOUD_API_TOKEN")
-
-    headers = [
-      {"Content-Type", "text/plain; charset=utf-8"},
-      {"Authorization", "Token #{token}"}
-    ]
-
-    %HTTPoison.Response{status_code: 204} =
-      HTTPoison.post!(
-        "https://europe-west1-1.gcp.cloud2.influxdata.com/api/v2/write?bucket=barragenspt&precision=s",
-        payload,
-        headers
-      )
-
-    # :timer.sleep(50)
-
-    :ok
+  defp save_rows(rows) do
+    Barragenspt.Repo.insert_all(DataPoint, rows)
   end
 end
