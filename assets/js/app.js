6803aba2aef3d48a4464a99ad292d547c4d8860b
Ricardo Paiva <ricardoccpaiva@gmail.com>
2022-02-28 21:23:59 +0000

    Use live view event listeners to handle data update and render chart

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,15 +22,15 @@ import "../css/app.css"
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", info => topbar.show())
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
 
@@ -43,3 +43,102 @@ liveSocket.connect()
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 
+window.addEventListener(`phx:update_chart`, (e) => {
+    const chart = new G2.Chart({
+        container: 'c1',
+        autoFit: true,
+        height: 300,
+        padding: [30, 20, 70, 30]
+    });
+
+    console.log(e.detail.data);
+
+    chart.data(e.detail.data);
+
+    chart.scale({
+        "Arade": {
+            min: 0,
+            max: 100
+        },
+        "Ave": {
+            min: 0,
+            max: 100
+        },
+        "Cávado": {
+            min: 0,
+            max: 100
+        },
+        "Douro": {
+            min: 0,
+            max: 100
+        },
+        "Guadiana": {
+            min: 0,
+            max: 100
+        },
+        "Lima": {
+            min: 0,
+            max: 100
+        },
+        "Mira": {
+            min: 0,
+            max: 100
+        },
+        "Mondego": {
+            min: 0,
+            max: 100
+        },
+        "Oeste": {
+            min: 0,
+            max: 100
+        },
+        "Ribeiras do Algarve": {
+            min: 0,
+            max: 100
+        },
+        "Sado": {
+            min: 0,
+            max: 100
+        },
+        "Tejo": {
+            min: 0,
+            max: 100
+        }
+    });
+    e.detail.lines.forEach(function (item) {
+        chart.axis(item.k, false);
+        chart.line().position("date*" + item.k).color(item.v);
+    });
+
+    chart.axis(e.detail.lines[0].k, true);
+    chart.render();
+})
+
+const loadDams = async () => {
+    const response = await fetch('/dam_coords');
+    const damsCoords = await response.json();
+    damsCoords.data.forEach(function (element) {
+        el = document.createElement('div');
+        innerHTML = "<a style='color:  " + element.basin_color + "'";
+        innerHTML = innerHTML + " class='fa fa-map-marker fa-2x' data-phx-link='patch'";
+        innerHTML = innerHTML + " data-phx-link-state='push' href='/dam/" + element.id + "'</a>";
+        el.innerHTML = innerHTML;
+
+        new mapboxgl
+            .Marker(el)
+            .setLngLat([element.lon, element.lat])
+            .addTo(map);
+    });
+}
+
+mapboxgl.accessToken = document.getElementById("mapbox_token").value;
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/ricardoccpaiva/ckzcpwm4h001114mn112tg6fr',
+    center: [-8, 39.69],
+    zoom: 6
+});
+
+loadDams();
+
+map.addControl(new mapboxgl.NavigationControl());
