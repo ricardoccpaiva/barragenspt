37aab2766e49a280243d64c8d2d2ff0f7ca1f2b0
Ricardo Paiva <ricardoccpaiva@gmail.com>
2022-02-28 21:25:02 +0000

    Refactor basin detail page
    - use real data
    - push event with data to update chart

--- lib/barragenspt_web/live/basin_detail_live.ex
+++ lib/barragenspt_web/live/basin_detail_live.ex
@@ -2,23 +2,40 @@ defmodule BarragensptWeb.BasinDetailLive do
   use BarragensptWeb, :live_view
   import Ecto.Query
   alias Barragenspt.Mappers.Colors
+  alias Barragenspt.Hydrometrics.Basins
+  alias Barragenspt.Hydrometrics.Stats
 
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, %{})}
-  end
+  def mount(%{"id" => id}, _session, socket) do
+    basin = Basins.get(id)
 
-  def handle_params(%{"id" => id}, _url, socket) do
     query = from p in Barragenspt.Hydrometrics.Dam, where: p.basin_id == ^String.to_integer(id)
 
     dams =
       query
       |> Barragenspt.Repo.all()
-      |> Enum.map(fn dam -> Map.put(dam, :pct, :rand.uniform(100)) end)
-      |> Enum.map(fn basin -> Map.put(basin, :pct_2, :rand.uniform(100)) end)
+      |> Enum.map(fn dam ->
+        {pct} = Stats.current_level_for_dam(dam.site_id)
+        Map.put(dam, :pct, pct)
+      end)
+      |> Enum.map(fn dam ->
+        {pct} = Stats.historical_level_for_dam(dam.site_id)
+        Map.put(dam, :pct_2, pct)
+      end)
       |> Enum.map(fn dam ->
         Map.put(dam, :capacity_color, Colors.lookup_capacity(dam.pct))
       end)
 
-    {:noreply, socket |> assign(dams: dams, basin: Enum.at(dams, 0).basin)}
+    {:ok, socket |> assign(dams: dams, basin: basin.basin)}
+  end
+
+  def handle_params(%{"id" => id}, _url, socket) do
+    {parsed_id, ""} = Integer.parse(id)
+    %{basin: basin_name} = basin = Basins.get(parsed_id)
+
+    data = Stats.for_basin(basin)
+
+    lines = [%{k: basin_name, v: Colors.lookup(id)}]
+
+    {:noreply, push_event(socket, "update_chart", %{data: data, lines: lines})}
   end
 end
