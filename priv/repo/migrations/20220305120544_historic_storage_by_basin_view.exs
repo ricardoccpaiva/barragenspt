14d32382e630f30692179142e7474dd335d90fb6
Ricardo Paiva <ricardoccpaiva@gmail.com>
2022-03-08 17:55:47 +0000

    Fix migrations

--- priv/repo/migrations/20220305120544_historic_storage_by_basin_view.exs
+++ priv/repo/migrations/20220305120544_historic_storage_by_basin_view.exs
@@ -4,14 +4,14 @@ defmodule Barragenspt.Repo.Migrations.HistoricStorageByBasinView do
   def up do
     execute("""
       CREATE VIEW basin_historic_storage AS
-      select extract as month, basin_id, avg(average) as value from(
-        select cast(extract(month from colected_at) as int), dp.dam_code, dp.basin_id, dp.site_id, (dp.value / cast(d.metadata  -> 'Albufeira' ->> 'Capacidade total (dam3)' as int))*100 as average
+      select month, basin_id, avg(average) as value from(
+        select cast(extract(month from colected_at) as int) as month, dp.dam_code, dp.basin_id, dp.site_id, (dp.value / cast(d.metadata  -> 'Albufeira' ->> 'Capacidade total (dam3)' as int))*100 as average
         from data_points dp inner join dam d
         on dp.site_id = d.site_id
         where param_name = 'volume_last_day_month'
         order by colected_at) as details
-      group by extract,basin_id
-      order by extract, basin_id;
+      group by month, basin_id
+      order by month, basin_id;
     """)
   end
 
