b21a737b445bc008334a7318e27783589136db58
Ricardo Paiva <ricardoccpaiva@gmail.com>
2022-03-09 21:59:09 +0000

    Add site current storage view

---
+++ lib/barragenspt/hydrometrics/site_current_storage.ex
@@ -0,0 +1,12 @@
+defmodule Barragenspt.Hydrometrics.SiteCurrentStorage do
+  use Ecto.Schema
+
+  @primary_key false
+  schema "site_current_storage" do
+    field :basin_id, :string
+    field :site_id, :string
+    field :site_name, :string
+    field :basin_name, :string
+    field :current_storage, :decimal
+  end
+end
