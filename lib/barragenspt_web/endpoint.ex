02457d48eed40510fe06f26486051f25591f626a
Ricardo Paiva <ricardoccpaiva@gmail.com>
2022-03-16 23:42:54 +0000

    Only use plug if not dev env

--- lib/barragenspt_web/endpoint.ex
+++ lib/barragenspt_web/endpoint.ex
@@ -10,45 +10,50 @@ defmodule BarragensptWeb.Endpoint do
     signing_salt: "5e2gUXHn"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket("/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]])
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
+  plug(Plug.Static,
     at: "/",
     from: :barragenspt,
     gzip: false,
     only: ~w(assets fonts images favicon.ico robots.txt geojson)
+  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
-    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :barragenspt
+    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
+    plug(Phoenix.LiveReloader)
+    plug(Phoenix.CodeReloader)
+    plug(Phoenix.Ecto.CheckRepoStatus, otp_app: :barragenspt)
   end
 
-  plug(PlugCanonicalHost, canonical_host: "barragens.pt")
+  if System.get_env("MIX_ENV") != "DEV" do
+    plug(PlugCanonicalHost, canonical_host: "barragens.pt")
+  end
 
-  plug PromEx.Plug, prom_ex_module: Barragenspt.PromEx
+  plug(PromEx.Plug, prom_ex_module: Barragenspt.PromEx)
 
-  plug Phoenix.LiveDashboard.RequestLogger,
+  plug(Phoenix.LiveDashboard.RequestLogger,
     param_key: "request_logger",
     cookie_key: "request_logger"
+  )
 
-  plug Plug.RequestId
-  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+  plug(Plug.RequestId)
+  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
 
-  plug Plug.Parsers,
+  plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
-  plug Plug.Session, @session_options
-  plug BarragensptWeb.Router
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
+  plug(Plug.Session, @session_options)
+  plug(BarragensptWeb.Router)
 end
