e9acf13daf000b59660614d9219aaca31f3d52e1
Ricardo Paiva <ricardoccpaiva@gmail.com>
2022-03-19 01:40:56 +0000

    Rename file

---
+++ lib/barragenspt/workers/stats_cacher.ex
@@ -0,0 +1,59 @@
+defmodule Barragenspt.Workers.StatsCacher do
+  use Oban.Worker, queue: :stats_cacher
+  alias Barragenspt.Hydrometrics.Dams
+  alias Barragenspt.Hydrometrics.Basins
+  require Logger
+
+  @impl Oban.Worker
+  def perform(_args) do
+    Barragenspt.Cache.flush()
+
+    dams = Dams.all()
+
+    basin_ids =
+      dams
+      |> Enum.uniq_by(fn %{basin_id: basin_id} -> basin_id end)
+      |> Enum.map(fn %{basin_id: basin_id} -> basin_id end)
+
+    daily_periods = [1, 6]
+    monthly_periods = [2, 5, 10, 50]
+
+    cache_basin_stats(basin_ids, daily_periods, monthly_periods)
+    cache_dams_stats(dams, daily_periods, monthly_periods)
+  end
+
+  defp cache_dams_stats(dams, daily_periods, monthly_periods) do
+    Enum.each(daily_periods, fn period ->
+      Enum.each(dams, fn dam ->
+        Dams.daily_stats(dam, period)
+      end)
+    end)
+
+    Enum.each(monthly_periods, fn period ->
+      Enum.each(dams, fn dam ->
+        Dams.monthly_stats(dam, period)
+      end)
+    end)
+  end
+
+  defp cache_basin_stats(basin_ids, daily_periods, monthly_periods) do
+    Basins.monthly_stats_for_basins()
+    Basins.summary_stats()
+
+    Enum.each(basin_ids, fn basin_id ->
+      Basins.summary_stats(basin_id)
+    end)
+
+    Enum.each(daily_periods, fn period ->
+      Enum.each(basin_ids, fn basin_id ->
+        Basins.daily_stats_for_basin(basin_id, period)
+      end)
+    end)
+
+    Enum.each(monthly_periods, fn period ->
+      Enum.each(basin_ids, fn basin_id ->
+        Basins.monthly_stats_for_basin(basin_id, period)
+      end)
+    end)
+  end
+end
