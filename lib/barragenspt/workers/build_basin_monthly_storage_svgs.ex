b66a03c50aca307c21c9aba1c252b4e6e70002f5
Ricardo Paiva <ricardoccpaiva@gmail.com>
2024-02-19 23:09:29 +0000

    Add worker to build basin storage svgs

---
+++ lib/barragenspt/workers/build_basin_monthly_storage_svgs.ex
@@ -0,0 +1,64 @@
+defmodule Barragenspt.Workers.BuildBasinMonthlyStorageSvgs do
+  use Oban.Worker, queue: :dams_info
+  require Logger
+  alias Barragenspt.Hydrometrics.Basins
+
+  @impl Oban.Worker
+  def perform(_args) do
+    basin_ids = [
+      "1551779250",
+      "107",
+      "1551779242",
+      "12",
+      "47",
+      "17",
+      "138",
+      "38",
+      "23",
+      "68",
+      "992",
+      "8"
+    ]
+
+    basins_svg_template =
+      "priv/static/svg/pt_basins_template.svg"
+      |> Path.expand()
+      |> File.read!()
+
+    all_basins_info_grouped_by_date =
+      basin_ids
+      |> Enum.map(fn bid -> Basins.monthly_stats_for_basin(bid, [], 60) end)
+      |> List.flatten()
+      |> Enum.group_by(fn b -> b.date end)
+
+    all_dates = Map.keys(all_basins_info_grouped_by_date)
+
+    Enum.each(all_dates, fn d ->
+      all_basins_info_grouped_by_date
+      |> Map.get(d)
+      |> build_svg(d, basins_svg_template)
+    end)
+
+    :ok
+  end
+
+  defp build_svg(info, date, template) do
+    templated_svg =
+      info
+      |> Enum.reject(fn i -> i.basin_id == "Média" end)
+      |> Enum.map(fn i ->
+        %{
+          from: "FILL_BASIN_#{i.basin_id}",
+          to: Barragenspt.Mappers.Colors.lookup_capacity(i.value)
+        }
+      end)
+      |> Enum.reduce(template, fn %{from: from, to: to}, acc ->
+        String.replace(acc, from, to)
+      end)
+
+    path =
+      "priv/static/images/basin_storage/svg/monthly/#{date.year}_#{date.month}.svg"
+
+    File.write!(path, templated_svg)
+  end
+end
