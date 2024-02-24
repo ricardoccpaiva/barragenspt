f9a1d7fc7631eecea86385bb7a3646761445535f
Ricardo Paiva <ricardoccpaiva@gmail.com>
2024-02-24 11:47:38 +0000

    Make legend mappings table generic for any meteo index

--- lib/barragenspt/hydrometrics/precipitation_legend_mapping.ex
+++ lib/barragenspt/hydrometrics/precipitation_legend_mapping.ex
@@ -1,17 +1,19 @@
-defmodule Barragenspt.Hydrometrics.PrecipitationLegendMapping do
+defmodule Barragenspt.Hydrometrics.LegendMapping do
   use Ecto.Schema
   import Ecto.Changeset
 
-  schema "precipitation_legend_mapping" do
+  schema "legend_mapping" do
+    field :meteo_index, :string
     field :color_hex, :string
     field :color_xyz, :string
-    field :mean_value, :decimal
+    field :min_value, :decimal
+    field :max_value, :decimal
   end
 
   @doc false
   def changeset(dam, attrs) do
     dam
-    |> cast(attrs, [:color_hex, :color_xyz, :mean_value])
-    |> validate_required([:color_hex, :color_xyz, :mean_value])
+    |> cast(attrs, [:meteo_index, :color_hex, :color_xyz, :min_value, :max_value])
+    |> validate_required([:color_hex, :color_xyz])
   end
 end
--- priv/repo/migrations/20240218171335_create_precipitation_legend_mappings.exs
+++ priv/repo/migrations/20240218171335_create_precipitation_legend_mappings.exs
@@ -1,11 +1,13 @@
-defmodule Barragenspt.Repo.Migrations.CreatePrecipitationLegendMappings do
+defmodule Barragenspt.Repo.Migrations.CreateLegendMappings do
   use Ecto.Migration
 
   def change do
-    create table(:precipitation_legend_mapping) do
+    create table(:legend_mapping) do
+      add :meteo_index, :string
       add :color_hex, :string
       add :color_xyz, :string
-      add :mean_value, :decimal
+      add :min_value, :decimal
+      add :max_value, :decimal
     end
   end
 end
