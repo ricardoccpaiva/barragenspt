d37e70b3e8fadf7aae093b2e19e0444e41c5973d
Ricardo Paiva <ricardoccpaiva@gmail.com>
2024-03-02 17:27:13 +0000

    Add more precipitation specs

--- assets/js/vega_lite_spec_constants.js
+++ assets/js/vega_lite_spec_constants.js
@@ -1,5 +1,6 @@
 export const pdsi_domain = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
 export const pdsi_range = ["#9c551f", "#b5773e", "#cfa263", "#e8cf90", "#e9ffbe", "#c7deb4", "#91bda8", "#5c9e9c", "#218291"];
+
 export const temperature_domain = [
     "0",
     "1",
@@ -27,7 +28,6 @@ export const temperature_domain = [
     "23",
     "24"
 ];
-
 export const temperature_range = [
     "#49006a",
     "#ae027e",
@@ -56,7 +56,8 @@ export const temperature_range = [
     "#6a0000"
 ];
 
-export const daily_precipitation_range = ["#000000", "#fde0dd", "#fcc5c0", "#fa9fb5", "#f768a1", "#de3497", "#ae027e", "#7a0277", "#753d8d", "#685e98", "#6a74a4", "#788db4", "#8da4c4", "#aec5dc", "#e0ecf4", "#f7fcfd", "#ffffff"]
+export const monthly_precipitation_range = ["#000000", "#fde0dd", "#fcc5c0", "#fa9fb5", "#f768a1", "#de3497", "#ae027e", "#7a0277", "#753d8d", "#685e98", "#6a74a4", "#788db4", "#8da4c4", "#aec5dc", "#e0ecf4", "#f7fcfd", "#ffffff"]
+export const monthly_precipitation_domain = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"];
 
-
-export const daily_precipitation_domain = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"];
+export const daily_precipitation_range = ["#360e38", "#88218c", "#753d8d", "#685e98", "#6a74a4", "#788db4", "#8da4c4", "#aec5dc", "#e0ecf4"]
+export const daily_precipitation_domain = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
