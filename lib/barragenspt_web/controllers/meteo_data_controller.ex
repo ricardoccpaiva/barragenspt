aa3350a8f96791321842c603a7fac9beca2b5197
Ricardo Paiva <ricardoccpaiva@gmail.com>
2024-03-02 17:23:48 +0000

    Add support for precipitation data

--- lib/barragenspt_web/controllers/meteo_data_controller.ex
+++ lib/barragenspt_web/controllers/meteo_data_controller.ex
@@ -6,6 +6,33 @@ defmodule BarragensptWeb.MeteoDataController do
   alias Barragenspt.Meteo.Precipitation
   alias Barragenspt.Meteo.Pdsi
 
+  def index(
+        conn,
+        args = %{
+          "year" => year,
+          "scale" => scale,
+          "grouped" => "true",
+          "meteo_index" => "precipitation"
+        }
+      ) do
+    year = String.to_integer(year)
+    scale = String.to_atom(scale)
+    month = args |> Map.get("month", "0") |> String.to_integer()
+
+    raw_data =
+      if month == 0 do
+        Precipitation.get_precipitation_data_by_scale(year, scale)
+      else
+        Precipitation.get_precipitation_data_by_scale(year, month, scale)
+      end
+
+    data = build_precipitation_csv(raw_data, year, month, scale)
+
+    conn
+    |> put_resp_content_type("text/csv")
+    |> send_resp(200, data)
+  end
+
   def index(conn, %{"year" => year, "month" => month, "meteo_index" => meto_index}) do
     year = String.to_integer(year)
     month = String.to_integer(month)
@@ -45,11 +72,6 @@ defmodule BarragensptWeb.MeteoDataController do
     |> send_resp(200, data)
   end
 
-  @decorate cacheable(
-              cache: Cache,
-              key: "precipitation_csv_data_#{year}",
-              ttl: 99_999_999
-            )
   defp build_csv(data, year) do
     data
     |> Enum.map(fn d -> "#{d.date},#{d.weighted_average}" end)
@@ -101,6 +123,70 @@ defmodule BarragensptWeb.MeteoDataController do
         |> Map.get(:index)
 
       "#{d.date},#{d.color_hex},#{d.weight},#{index}"
+    end)
+    |> Enum.join("\n")
+    |> Kernel.then(fn v -> "#{header}\n#{v}" end)
+  end
+
+  @decorate cacheable(
+              cache: Cache,
+              key: "precipitation_csv_data_#{year}_#{month}_#{variant}",
+              ttl: 999
+            )
+  def build_precipitation_csv(data, year, month \\ 0, variant) do
+    colors_index =
+      if month != 0 do
+        [
+          %{color_hex: "#360e38", index: 0},
+          %{color_hex: "#88218c", index: 1},
+          %{color_hex: "#753d8d", index: 2},
+          %{color_hex: "#685e98", index: 3},
+          %{color_hex: "#6a74a4", index: 4},
+          %{color_hex: "#788db4", index: 5},
+          %{color_hex: "#8da4c4", index: 6},
+          %{color_hex: "#aec5dc", index: 7},
+          %{color_hex: "#e0ecf4", index: 8}
+        ]
+      else
+        [
+          %{index: 1, color_hex: "#000000"},
+          %{index: 2, color_hex: "#fde0dd"},
+          %{index: 3, color_hex: "#fcc5c0"},
+          %{index: 4, color_hex: "#fa9fb5"},
+          %{index: 5, color_hex: "#f768a1"},
+          %{index: 6, color_hex: "#de3497"},
+          %{index: 7, color_hex: "#ae027e"},
+          %{index: 8, color_hex: "#7a0277"},
+          %{index: 9, color_hex: "#753d8d"},
+          %{index: 10, color_hex: "#685e98"},
+          %{index: 11, color_hex: "#6a74a4"},
+          %{index: 12, color_hex: "#788db4"},
+          %{index: 13, color_hex: "#8da4c4"},
+          %{index: 14, color_hex: "#aec5dc"},
+          %{index: 15, color_hex: "#e0ecf4"},
+          %{index: 16, color_hex: "#f7fcfd"},
+          %{index: 17, color_hex: "#ffffff"}
+        ]
+      end
+
+    header = "date,color_hex,value,index"
+
+    data
+    |> Enum.map(fn d ->
+      if !Map.has_key?(d, :date) do
+        dt = Date.new!(d.year, d.month, 1)
+        Map.put(d, :date, dt)
+      else
+        d
+      end
+    end)
+    |> Enum.map(fn d ->
+      index =
+        colors_index
+        |> Enum.find(fn c -> c.color_hex == d.color_hex end)
+        |> Map.get(:index)
+
+      "#{d.date},#{d.color_hex},#{d.value},#{index}"
     end)
     |> Enum.join("\n")
     |> Kernel.then(fn v -> "#{header}\n#{v}" end)
