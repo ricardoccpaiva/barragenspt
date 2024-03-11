6821acc007b5be6eb130d23f62537b4efadc96c8
Ricardo Paiva <ricardoccpaiva@gmail.com>
2024-03-11 23:09:23 +0000

    Change reports controller to point to assets.barragens.pt

--- lib/barragenspt_web/controllers/reports_controller.ex
+++ lib/barragenspt_web/controllers/reports_controller.ex
@@ -245,7 +245,7 @@ defmodule BarragensptWeb.ReportsController do
     abbreviated_year = String.slice("#{year}", -2, 2)
 
     %{
-      url: "images/#{meteo_index}/svg/monthly/#{year}_#{month}.svg",
+      url: "https://assets.barragens.pt/#{meteo_index}/svg/monthly/minified/#{year}_#{month}.svg",
       date: "#{month_name} '#{abbreviated_year}",
       year: year
     }
@@ -261,9 +261,9 @@ defmodule BarragensptWeb.ReportsController do
 
     url =
       if variant in ["min", "max"] do
-        "images/#{meteo_index}/svg/daily/#{y}_#{mx}_#{dx}_#{variant}.svg"
+        "https://assets.barragens.pt/#{meteo_index}/svg/daily/minified/#{y}_#{mx}_#{dx}_#{variant}.svg"
       else
-        "images/#{meteo_index}/svg/daily/#{y}_#{mx}_#{dx}.svg"
+        "https://assets.barragens.pt/#{meteo_index}/svg/daily/minified/#{y}_#{mx}_#{dx}.svg"
       end
 
     %{
