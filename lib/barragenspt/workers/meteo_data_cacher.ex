1dd3ebd4d74f1d92c044cafd7dacf85a9f0b1697
Ricardo Paiva <ricardoccpaiva@gmail.com>
2024-03-14 21:22:12 +0000

    Improve meteo data cacher

--- lib/barragenspt/workers/meteo_data_cacher.ex
+++ lib/barragenspt/workers/meteo_data_cacher.ex
@@ -4,18 +4,60 @@ defmodule Barragenspt.Workers.MeteoDataCacher do
   def spawn_workers() do
     Barragenspt.Cache.flush()
 
+    sd = Date.new!(2000, 1, 1)
+    ed = Date.utc_today()
+    dates = Date.range(sd, ed)
+    dates = Enum.filter(dates, fn dt -> dt.day == 1 end)
+
     combinations =
-      for year <- 2000..2023,
-          month <- 1..12,
+      for date <- dates,
           layer <- ["min", "max"],
-          do: {year, month, layer}
+          do: {date.year, date.month, layer}
+
+    combinations_precipitation =
+      for date <- dates,
+          do: {date.year, date.month}
 
     jobs =
       Enum.map(combinations, fn {year, month, layer} ->
         Barragenspt.Workers.MeteoDataCacher.new(%{
           "year" => year,
           "month" => month,
-          "layer" => layer
+          "layer" => layer,
+          "meteo_index" => "temperature"
+        })
+      end)
+
+    Oban.insert_all(jobs)
+
+    years = Enum.uniq(combinations_precipitation, fn {year, _month} -> year end)
+
+    jobs =
+      Enum.map(years, fn {year, _month} ->
+        Barragenspt.Workers.MeteoDataCacher.new(%{
+          "year" => year,
+          "meteo_index" => "pdsi"
+        })
+      end)
+
+    Oban.insert_all(jobs)
+
+    jobs =
+      Enum.map(years, fn {year, _month} ->
+        Barragenspt.Workers.MeteoDataCacher.new(%{
+          "year" => year,
+          "meteo_index" => "precipitation"
+        })
+      end)
+
+    Oban.insert_all(jobs)
+
+    jobs =
+      Enum.map(combinations_precipitation, fn {year, month} ->
+        Barragenspt.Workers.MeteoDataCacher.new(%{
+          "year" => year,
+          "month" => month,
+          "meteo_index" => "precipitation"
         })
       end)
 
@@ -23,8 +65,36 @@ defmodule Barragenspt.Workers.MeteoDataCacher do
   end
 
   @impl Oban.Worker
-  def perform(%Oban.Job{args: %{"year" => year, "month" => month, "layer" => layer}}) do
-    Barragenspt.Meteo.Temperature.get_temperature_data_by_scale(year, month, layer)
+  def perform(%Oban.Job{
+        args: %{
+          "year" => year,
+          "month" => month,
+          "layer" => layer,
+          "meteo_index" => "temperature"
+        }
+      }) do
+    Barragenspt.Meteo.Temperature.get_data_by_scale(year, month, layer)
+    :ok
+  end
+
+  def perform(%Oban.Job{
+        args: %{"year" => year, "month" => month, "meteo_index" => "precipitation"}
+      }) do
+    Barragenspt.Meteo.Precipitation.get_precipitation_data(year, month)
+    :ok
+  end
+
+  def perform(%Oban.Job{
+        args: %{"year" => year, "meteo_index" => "precipitation"}
+      }) do
+    Barragenspt.Meteo.Precipitation.get_monthly_precipitation_data_by_scale(year)
+    Barragenspt.Meteo.Precipitation.get_precipitation_data(year)
+    :ok
+  end
+
+  def perform(%Oban.Job{
+        args: %{"year" => year, "meteo_index" => "pdsi"}
+      }) do
     Barragenspt.Meteo.Pdsi.get_pdsi_data_by_scale(year)
     :ok
   end
