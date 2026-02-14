ab0e5701281f671209bb6cb2936ea295b8b56bf2
Ricardo Paiva <ricardoccpaiva@gmail.com>
2026-02-14 22:12:45 +0000

    Remove unused files

--- lib/barragenspt_web/controllers/meteo_data_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule BarragensptWeb.MeteoDataController do
-  use BarragensptWeb, :controller
-
-  def index(conn, _params) do
-    redirect(conn, to: "/v2")
-  end
-end
--- lib/barragenspt_web/controllers/reports_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule BarragensptWeb.ReportsController do
-  use BarragensptWeb, :controller
-
-  def index(conn, _params) do
-    redirect(conn, to: "/v2")
-  end
-end
