2d9debef0a3639798c110b229ea87dd56d4d3fe0
Ricardo Paiva <ricardoccpaiva@gmail.com>
2026-05-11 21:27:54 +0100

    Move drawers to its own component files

--- lib/barragenspt_web/components/storage_report_components.ex
+++ lib/barragenspt_web/components/storage_report_components.ex
@@ -3,4 +3,10 @@ defmodule BarragensptWeb.StorageReportComponents do
   use BarragensptWeb, :component
 
   embed_templates "storage_report_components/*"
+
+  defp period_badge("monthly", selected_month, _selected_date),
+    do: "Mês de #{Calendar.strftime(selected_month, "%Y-%m")}"
+
+  defp period_badge(_report_type, _selected_month, selected_date),
+    do: "Semana de #{Calendar.strftime(selected_date, "%Y-%m-%d")}"
 end
